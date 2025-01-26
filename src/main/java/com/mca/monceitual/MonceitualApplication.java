fb684878465ca349cbbd3ebaca074ea2291cd422
MARCILIO369 <marcilioclaudinoaraujo@gmail.com>
2025-01-26 17:04:05 -0300

    Projeto Criado

---
+++ src/main/java/com/mca/monceitual/MonceitualApplication.java
@@ -0,0 +1,13 @@
+package com.mca.monceitual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonceitualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonceitualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mca/monceitual/MonceitualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mca.monceitual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonceitualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
