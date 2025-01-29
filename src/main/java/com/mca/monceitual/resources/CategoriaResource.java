168831e9546aa3d68be552e489a2ac6d22621e8e
Marcilio Claudino <marcilioclaudinoaraujo@gmail.com>
2025-01-29 16:49:09 -0300

    Testando a primeira classe de dominio

--- src/main/java/com/mca/monceitual/resources/CategoriaResource.java
+++ src/main/java/com/mca/monceitual/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
 package com.mca.monceitual.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mca.monceitual.domain.Categoria;
 
 
 @RestController  //Diz que é um controlador REST
@@ -10,7 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET) // Associou ao verbo HTTP
-	public String listar() {
-	return "Rest esta funcionando !";
-}
+	public List<Categoria> listar() {
+		
+	Categoria cat1 = new Categoria(1, "Informatica");	
+	Categoria cat2 = new Categoria(2, "Escritorio");
+	
+	List<Categoria> lista = new ArrayList<>();
+	lista.add(cat1);
+	lista.add(cat2);
+	
+	
+	return lista; 
+	
+	}
 }
